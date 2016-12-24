dd730814690816409dea319debe5951bdf002637
Ralph Potter <ralph@rendering.io>
2016-12-24 15:58:30 +0000

    Added pipeline stage enum got commands that require masks.

--- include/vk/vk.h
+++ include/vk/vk.h
@@ -43,6 +43,33 @@ enum class index_type {
   uint32
 };
 
+// Explicitly binary compatible with VkPipelineStageFlagBits
+enum class pipeline_stage {
+  top_of_pipe             = 0x00000001,
+  draw_indirect           = 0x00000002,
+  vertex_input            = 0x00000004,
+  vertex_shader           = 0x00000008,
+  tessellation_control    = 0x00000010,
+  tessellation_evaluation = 0x00000020,
+  geometry_shader         = 0x00000040,
+  fragment_shader         = 0x00000080,
+  early_fragment_tests    = 0x00000100,
+  late_fragment_tests     = 0x00000200,
+  color_attachment_output = 0x00000400,
+  compute_shader          = 0x00000800,
+  transfer                = 0x00001000,
+  bottom_of_pipe          = 0x00002000,
+  host                    = 0x00004000,
+  all_graphics            = 0x00008000,
+  all_commands            = 0x00010000,
+  command_process_nvx     = 0x00020000,
+};
+
+inline pipeline_stage operator|(pipeline_stage lhs, pipeline_stage rhs) {
+  using T = std::underlying_type_t<pipeline_stage>;
+  return static_cast<pipeline_stage>(static_cast<T>(lhs) | static_cast<T>(rhs));
+}
+
 template<typename I>
 class iterator_range : std::pair<I, I> {
 public:
